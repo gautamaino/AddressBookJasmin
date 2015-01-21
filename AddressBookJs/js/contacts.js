f26ddbb95c8ddbbaca9a9cab2560aa5330cdabd0
Ravindra <ravindra@iternia.com>
2015-01-21 16:11:46 +0530

    edit and delete of record added.

--- AddressBookJs/js/contacts.js
+++ AddressBookJs/js/contacts.js
@@ -10,15 +10,25 @@ Contacts.prototype.addContact = function(contact){
 };
 
 // delete contact
-Contacts.prototype.deleteContact = function(contact){
-	var pos = this.contactList.indexOf(contact) ;
-	contactList.splice(pos , 1) ;
+//Contacts.prototype.deleteContact = function(contact){
+//	var pos = this.contactList.indexOf(contact) ;
+//	contactList.splice(pos , 1) ;
+//};
+Contacts.prototype.deleteContact = function(position){
+	//var pos = this.contactList.indexOf(contact) ;
+	this.contactList.splice(position , 1) ;
 };
 
 // update contact
-Contacts.prototype.updateContact = function(contact){
+Contacts.prototype.updateContact = function(position, contact){
 	//var pos = this.contactList.indexOf(contact) ;
 	//contactList.splice(pos , 1) ;
+	this.contactList[position] = contact;
 };
 
+// show contact
+Contacts.prototype.showContact = function(position){
+	//var pos = this.contactList.indexOf(contact) ;
+	return this.contactList[position] ;
+};
 
--- AddressBookJs/js/main.js
+++ AddressBookJs/js/main.js
@@ -2,11 +2,16 @@
 var allContactList = new Contacts();
 $("#recordGrid").hide();
 $("#newContactForm").hide();
+var editContactId = null;
+
 $( document ).ready(function() {
     console.log( "ready!" );
 
     // event on form submit
-    $("#contactForm").submit(function( event ){
+    $("#saveBtn").click(function( event ){
+
+
+
         console.log( "form submit event" );
 
         name = $("#userName").val();
@@ -16,12 +21,13 @@ $( document ).ready(function() {
 
         var new_contact = new Contact(name, email, phone, address);
 
-        //console.log(new_contact);
-        console.log(new_contact.validate());
-        console.log(new_contact);
-
         if (new_contact.validate()){
-            allContactList.addContact(new_contact);
+            if (editContactId == null){
+                allContactList.addContact(new_contact);
+            }else{
+                allContactList.updateContact(editContactId, new_contact);
+            }
+            //allContactList.addContact(new_contact);
         }
 
         console.log(allContactList);
@@ -36,9 +42,6 @@ $( document ).ready(function() {
         $("#newContactForm").hide();
         $("#recordGrid").show();
 
-        //var allRecords = "";
-
-        //console.log(allRecords);
         $("#recordTable tbody").html("");
 
         $.each(allContactList.contactList, function(index, val){
@@ -48,29 +51,64 @@ $( document ).ready(function() {
             "<td>"+val.email+"</td>"+
             "<td>"+val.phone+"</td>"+
             "<td>"+val.address+"</td>"+
-            "<td>"+index+"</td>"+
-            "<td>"+index+"</td>"+
+            "<td><button type='button' class='editRecord' value="+index+">Edit</button></td>"+
+            "<td><button type='button' class='deleteRecord' value="+index+">Delete</button></td>"+
             "</tr>";
 
-            //$("#recordTable tbody").html(allRecords);
             $("#recordTable tbody").append(allRecords);
-            //allRecords += allRecords +"<tr>" +
-            //"<td>"+val.name+"</td>"+
-            //"<td>"+val.email+"</td>"+
-            //"<td>"+val.phone+"</td>"+
-            //"<td>"+val.address+"</td>"+
-            //"<td>"+index+"</td>"+
-            //"<td>"+index+"</td>"+
-            //"</tr>";
+
         });
 
-        //console.log(allRecords);
+
+        // bind click event on dynamically created buttons
+        // edit button event
+        $(".editRecord").on ("click" ,function( event ){
+            console.log("edit button clicked");
+            console.log(this.value);
+
+            $("#newContactForm").show();
+            $("#recordGrid").hide();
 
 
-        //$("#recordTable tbody").html(allRecords);
+            var edirContact = allContactList.showContact(this.value);
 
 
-        console.log("inside show div");
+            $("#userName").val(edirContact.name);
+            $("#userEmail").val(edirContact.email);
+            $("#userContact").val(edirContact.phone);
+            $("#userAddress").val(edirContact.address);
+
+
+            editContactId = this.value
+
+        });
+
+
+        // delete button event
+        $(".deleteRecord").on ("click" ,function( event ){
+            console.log("delete button clicked");
+            console.log(this.value);
+            allContactList.deleteContact(this.value);
+
+            $("#recordTable tbody").html("");
+
+            $.each(allContactList.contactList, function(index, val){
+
+                var allRecords = "<tr>" +
+                    "<td>"+val.name+"</td>"+
+                    "<td>"+val.email+"</td>"+
+                    "<td>"+val.phone+"</td>"+
+                    "<td>"+val.address+"</td>"+
+                    "<td><button type='button' class='editRecord' value="+index+">Edit</button></td>"+
+                    "<td><button type='button' class='deleteRecord' value="+index+">Delete</button></td>"+
+                    "</tr>";
+
+                $("#recordTable tbody").append(allRecords);
+
+            });
+        });
+
+        //console.log("inside show div");
 
         event.preventDefault();
     });
@@ -79,11 +117,19 @@ $( document ).ready(function() {
     $("#addNewContact").click(function( event ){
         $("#newContactForm").show();
         $("#recordGrid").hide();
+        editContactId = null;
 
         console.log("inside show div ..... ");
 
         event.preventDefault();
     });
+
+    //
+    //$(".editRecord").on ("click" ,function( event ){
+    //    console.log("edit button clicked");
+    //});
+
+
 
 
 
